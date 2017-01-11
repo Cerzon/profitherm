a08a53b6e519604924a54a760714a9ed6cf48591
Cerzon <kirill.tkachenko@gmail.com>
2017-01-11 03:25:37 +0300

    start

--- sitepages/models.py
+++ sitepages/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class StaticPage(models.Model):
-    page_name = models.CharField(max_length=80)
-    meta_description = models.CharField(max_length=255)
-    meta_keywords = models.CharField(max_length=255)
-    date_created = models.DateTimeField()
+    page_name = models.CharField(max_length=30)
+    order_num = models.
+    meta_description = models.CharField(max_length=200)
+    meta_keywords = models.CharField(max_length=160)
+    date_created = models.DateField()
