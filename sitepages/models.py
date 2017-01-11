c0bdd8593e12fd19abec5c33e992a88580e27176
Cerzon <kirill.tkachenko@gmail.com>
2017-01-11 03:28:27 +0300

    test

--- sitepages/models.py
+++ sitepages/models.py
@@ -3,8 +3,14 @@ from django.db import models
 # Create your models here.
 
 class StaticPage(models.Model):
+    order_num = models.IntegerField()
     page_name = models.CharField(max_length=30)
-    order_num = models.
+    menu_text = models.CharField(max_length=30)
+    is_published = models.BooleanField(default=False)
     meta_description = models.CharField(max_length=200)
     meta_keywords = models.CharField(max_length=160)
-    date_created = models.DateField()
+    date_created = models.DateField(auto_now_add=True)
+    page_data = models.TextField()
+
+    class Meta():
+        ordering = ['order_num']
