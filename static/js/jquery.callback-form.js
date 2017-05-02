0a3c57e7f4c7b0a830d3016568220cd883293f79
Cerzon <kirill.tkachenko@gmail.com>
2017-05-02 17:08:14 +0300

    2017-05-02-17-08

--- static/js/jquery.callback-form.js
+++ static/js/jquery.callback-form.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
     $('#get-callback').on('click', function(e){
         e.preventDefault();
-        $.get("callback/", function(data){
-            $('#ptcb-content').append($(data));
+        $.ajax({
+            url : "/callback/",
+            type : "GET",
+            data : {},
+            success : function(data) {
+                $('#ptcb-content').append($(data));
+                $('#callback-form').on('submit', function(e){
+                    e.preventDefault();
+                    postCallback();
+                });
+                $('#ptcb-overlay, #ptcb-content, #ptcb-close').show();
+            }
         });
-        $('#ptcb-overlay, #ptcb-content, #ptcb-close').show();
     });
     $('#ptcb-close').on('click', function(){
         $('#ptcb-content').children('form').remove();
         $('#ptcb-overlay, #ptcb-content, #ptcb-close').hide();
     });
 });
+
+function postCallback() {
+    console.log('callback form submit function');
+}
