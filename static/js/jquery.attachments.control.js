5c6d728f9bcfda6b548d7445223aa37e1404db5c
Cerzon <kirill.tkachenko@gmail.com>
2017-05-17 01:11:11 +0300

    2017-05-17-01-11

--- static/js/jquery.attachments.control.js
+++ static/js/jquery.attachments.control.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
     var $att_forms = [];
     var $form_ready = true;
+    var $fake_change = false;
     var $forms_showed = 1;
     $('.inline-form').each(function(){
-        $att_forms.push($(this));
-        $(this).hide();
+        $att_forms.push($(this).hide());
     });
-    $att_forms.pop().show();
+    $att_forms.reverse().pop().show();
     $('.clear-field').on('click', function(e){
         e.stopPropagation();
         e.preventDefault();
+        $fake_change = true;
         $fieldId = $(this).data('fieldId');
-        $($fieldId).val('').show();
-        $($fieldId + '-filename').remove();
         $(this).hide();
+        $($fieldId + '-filename').remove();
+        $($fieldId).val('').show();
         if ($form_ready && $forms_showed > 1) {
-            $att_forms.push($(this).closest('.inline-form'));
-            $(this).closest('.inline-form').hide();
+            $att_forms.push($(this).closest('.inline-form').hide());
             $forms_showed -= 1;
             if (!Modernizr.flexbox) tuneHeight();
         }
@@ -27,16 +27,19 @@ $(document).ready(function(){
             $($fieldId).wrap('<form></form>').closest('form').get(0).reset();
             $($fieldId).unwrap();
         }
+        $fake_change = false;
     });
     $('.inline-form').children('input[type="file"]').on('change', function(){
-        $('<span id="' + this.id + '-filename">' + $(this).val() + '</span>').insertAfter(this);
-        $(this).closest('.inline-form').children('a.clear-field').show();
-        $(this).hide();
-        if ($att_forms.length) {
-            $att_forms.pop().show();
-            $forms_showed += 1;
-            if (!Modernizr.flexbox) tuneHeight();
+        if (!$fake_change) {
+            $('<span id="' + this.id + '-filename">' + $(this).val().split(/[\\/]/).pop() + '</span>').insertAfter(this);
+            $(this).closest('.inline-form').children('a.clear-field').show();
+            $(this).hide();
+            if ($att_forms.length) {
+                $att_forms.pop().show();
+                $forms_showed += 1;
+                if (!Modernizr.flexbox) tuneHeight();
+            }
+            if (!$att_forms.length) $form_ready = false;
         }
-        if (!$att_forms.length) $form_ready = false;
     });
 });
