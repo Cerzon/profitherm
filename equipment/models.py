7883d70353bccbb0e2fc054de37d9671ba80a492
Cerzon <kirill.tkachenko@gmail.com>
2017-10-16 01:18:16 +0300

    equip20171016-0118

--- equipment/models.py
+++ equipment/models.py
@@ -4,10 +4,17 @@ from django.db import models
 
 class StockKeepingUnit(models.Model):
     "Единица товарного учёта"
-    sku_name = models.CharField(max_length=16, verbose_name='название', help_text='должно быть уникальным')
+    sku_name = models.CharField(max_length=16, verbose_name='название')
     sku_displayname = models.CharField(max_length=30, verbose_name='отображаемое название', help_text='включая html-тэги типа <sup></sup> и т.п.')
-    price_multiplier = models.FloatField(default=1, verbose_name='умножающий коэффициент', help_text='множитель базовой цены товара')
-    discount = models.SmallIntegerField(default=0, verbose_name='скидка/наценка', help_text='в процентах, скидка или наценка (если отрицательное значение) для данной единицы')
+
+    class Meta():
+        verbose_name = 'товарная единица'
+        verbose_name_plural = 'товарные единицы'
+
+
+class PriceCurrency(models.Model):
+    "Валюта"
+    pass
 
 
 class BrandName(models.Model):
@@ -15,8 +22,13 @@ class BrandName(models.Model):
     pass
 
 
-class Manufacturer(models.Model):
-    "Производитель"
+class ManufacturerHomeland(models.Model):
+    "Страна производитель"
+    pass
+
+
+class EquipmentCategory(models.Model):
+    "Категория оборудования"
     pass
 
 
@@ -27,6 +39,11 @@ class BaseProduct(models.Model):
     vendor_code = models.CharField(max_length=40, verbose_name='артикул')
     product_name = models.CharField(max_length=120, verbose_name='наименование')
     product_fullname = models.TextField(blank=True, verbose_name='полное наименование')
+    product_price = models.FloatField(verbose_name='каталожная цена')
+    price_unit = models.ForeignKey(StockKeepingUnit, on_delete=models.PROTECT, related_name='+')
+    price_currency = models.ForeignKey(PriceCurrency, on_delete=models.PROTECT, related_name='product_set')
+    trade_unit = models.ForeignKey(StockKeepingUnit, on_delete=models.PROTECT, related_name='+')
+    trade_unit_multiplier = models.PositiveSmallIntegerField(default=1, verbose_name='кратность единицы продажи')
 
     class Meta():
         abstract = True
