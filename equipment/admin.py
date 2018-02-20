579f589e4150b049d30cc7b70bf98c6e94fb9e67
Cerzon <kirill.tkachenko@gmail.com>
2018-02-20 18:05:46 +0300

    equip20180220_1805

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Radiator, Pump, PumpControl, Boiler, ControlPanel, ExtensionControlModule, Sensor, WaterHeater, ExpansionVessel, StockKeepingUnit, PriceCurrency, BrandName, Manufacturer
+from .models import Radiator, Pump, PumpControl, Boiler, ControlPanel, ExtensionControlModule, Sensor, WaterHeater, ExpansionVessel, StockKeepingUnit, PriceCurrency, BrandName, Manufacturer, EquipmentCategory
 
 # Register your models here.
 admin.site.register(Radiator)
@@ -15,3 +15,4 @@ admin.site.register(StockKeepingUnit)
 admin.site.register(PriceCurrency)
 admin.site.register(BrandName)
 admin.site.register(Manufacturer)
+admin.site.register(EquipmentCategory)
